ade0222a382da8f9481c312b4df78c2814a0c01a
Thiago Domingos <thi.domsilva@gmail.com>
2025-09-27 14:58:56 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/useSignals.tsx
+++ src/hooks/useSignals.tsx
@@ -14,15 +14,23 @@ export function useSignals(
   const [rows, setRows] = useState<Signal[]>([]);
 
   useEffect(() => {
-    setLoading(true);
+    let isMounted = true;
     setError(undefined);
 
     if (source === 'mock') {
       const stop = startMockStream((data) => {
-        setRows(data);
-        if (loading) setLoading(false);
+        if (isMounted) {
+          setRows(data);
+          if (loading) {
+            setLoading(false);
+          }
+        }
       }, refreshInterval);
-      return () => stop();
+
+      return () => {
+        isMounted = false;
+        stop();
+      };
     }
 
     // Firestore (ativar quando o back estiver pronto)
@@ -33,16 +41,23 @@ export function useSignals(
     //   limit(100)
     // );
     // const unsub = onSnapshot(q, (snap)=>{
-    //   const data: Signal[] = [];
-    //   snap.forEach(d => data.push({ id:d.id, ...(d.data() as any) }));
-    //   setRows(data);
-    //   setLoading(false);
+    //   if(isMounted) {
+    //     const data: Signal[] = [];
+    //     snap.forEach(d => data.push({ id:d.id, ...(d.data() as any) }));
+    //     setRows(data);
+    //     setLoading(false);
+    //   }
     // }, (e)=>{
-    //   setError(e.message);
-    //   setLoading(false);
+    //   if(isMounted) {
+    //     setError(e.message);
+    //     setLoading(false);
+    //   }
     // });
-    // return () => unsub();
-  }, [source, loading, refreshInterval]);
+    // return () => {
+    //   isMounted = false;
+    //   unsub();
+    // };
+  }, [source, refreshInterval]);
 
   return { loading, error, rows };
 }
